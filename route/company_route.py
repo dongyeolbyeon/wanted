f285b233ee01f2ed8b9476808e53b75b9412dbe6
dongyeol.byeon <dongbest79@naver.com>
2024-12-14 10:02:47 +0900

    api route 수정

--- route/company_route.py
+++ route/company_route.py
@@ -21,9 +21,6 @@ class WantedCompanies(Resource):
         """회사명 조회"""
         return company_service.search_company(request.args.get('company_name'))
 
-
-@api.route('')
-class WantedCompaniesChange(Resource):
     parser = api.parser()
     parser.add_argument('company_id', type=int, help='company key', required=True)
     parser.add_argument('col_target', type=LangEnum, help='추가 TAG Target(ko, en, ja)', required=True)
@@ -32,9 +29,11 @@ class WantedCompaniesChange(Resource):
     @api.expect(parser)
     def put(self):
         """테그 추가 """
-        return company_service.update_company(request.args.get('company_id'), request.args.get('col_target'), request.args.get('company_tag'))
+        return company_service.update_company(request.args.get('company_id'), request.args.get('col_target'),
+                                              request.args.get('company_tag'))
 
     @api.expect(parser)
     def delete(self):
         """테그 삭제"""
-        return company_service.delete_company(request.args.get('company_id'), request.args.get('col_target'), request.args.get('company_tag'))
+        return company_service.delete_company(request.args.get('company_id'), request.args.get('col_target'),
+                                              request.args.get('company_tag'))
